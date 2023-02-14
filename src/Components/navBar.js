c6ef307c86bdea0e969ca015b8facdeeea978f54
ryjtoh <ryjtoh@gmail.com>
2023-02-14 00:35:45 -0800

    tried to get the logo and title in the navbar working

--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -10,13 +10,21 @@ const NavBarStyle = {
   height: "50px",
   justifyContent: "left",
   borderRadius: "0px 0px 15px 15px",
-  overflow: "hidden"
+  overflow: "hidden",
+  color: "white",
+  paddingLeft: "50px"
 }
 
 export default function NavBar() {
   return (
     <div className='nav-container'> 
-      <div className='nav-links' style={NavBarStyle}>
+      <div className='nav-links' style={NavBarStyle}> 
+        <Link to='/'>
+          <div style={{ display: "flex"}}> {/* TODO: Fill out inline styles for the logo + title */}
+            <img src='../../public/compostlogo.png' alt='compostlogo'></img> {/* Idk why this doesn't work */}
+            <h2>Sortable</h2>
+          </div>
+        </Link>
         <Link to='/'>Home</Link>
         <Link to='/CompostBins'>Compost Bins</Link>
         <Link to='/RssFeed'>RSS Feed</Link>
