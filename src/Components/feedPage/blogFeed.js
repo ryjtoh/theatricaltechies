a136a4a1800a0c734cc65114030cd988c6b475cc
Evan Siu <evansiu7@gmail.com>
2023-03-05 20:48:22 -0800

    make images work

--- src/Components/feedPage/blogFeed.js
+++ src/Components/feedPage/blogFeed.js
@@ -13,7 +13,7 @@ const Img = styled('img')({
 const blogData = [
   {
     id: 1,
-    imageSrc: './images/BioCycle.jpeg',
+    imageSrc: 'blogImages/UW Sustainability.jpeg',
     title: ' UW is ready for Seattle\'s new composting law',
     summary: ' The Daily took a look at what Seattle\'s new composting ordinanice means for the University of Washington.',
     source: <a href="https://sustainability.uw.edu/blog/2015-01/uw-ready-seattles-new-composting-law"> UW Sustainability</a>,
@@ -21,7 +21,7 @@ const blogData = [
   },
   {
     id: 2,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/BioCycle.jpeg',
     title: ' Seattle\'s Winning Strategy For Managing Organics',
     summary: ' When programs, garbage collection fees, or regulations cause households to move organics out of the garbage cart and into the organics cart, costs for managing MSW go down.',
     source: <a href="https://www.biocycle.net/seattles-winning-strategy-managing-organics/"> BioCycle</a>,
@@ -29,7 +29,7 @@ const blogData = [
   },
   {
     id: 3,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/UWNews.jpeg',
     title: ' WA Dep of Ecology "Compost" Data References',
     summary: ' Composting is an important component of "closed loop" recycling. We set regulatory standards for composting facilities and compost quality and also provide technical assistance to local governments',
     source: <a href="https://ecology.wa.gov/Waste-Toxics/Reducing-recycling-waste/Waste-reduction-programs/Organic-materials/Managing-organics-compost"> WA Dep of Ecology </a>,
@@ -37,7 +37,7 @@ const blogData = [
   },
   {
     id: 4,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/Rethink Green Seattle.jpeg',
     title: ' Composting in Seattle is Easy. And Required.',
     summary: ' Moving to Seattle was like a dream come true, in terms of composting. Curbside pick-up of kitchen scraps and yard waste every week?!',
     source: <a href="http://re-thinkgreen.com/2016/04/13/municipal-composting-seattle/"> Re-think Green </a>,
@@ -45,7 +45,7 @@ const blogData = [
   },
   {
     id: 5,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/Sqwatch-secrets.jpeg',
     title: ' Sqwatch\'s secret #8: Compost, compost, compost!',
     summary: ' We\'re lucky in Seattle to have city-wide composting, which makes it easy to keep food scraps out of the landfill. Choosing compost instead of the landfill bin reduces methane emissions and allows for the material to be reused to increase soil nutrients.',
     source: <a href="https://sustainability.uw.edu/blog/2022-12/sqwatchs-secret-8-compost-compost-compost"> UW Sustainability </a>,
@@ -53,7 +53,7 @@ const blogData = [
   },
   {
     id: 6,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/Foodable.jpeg',
     title: ' Seattle\'s Mandated Composting Program Enlists Restaurants to Reduce Waste',
     summary: ' In 2015, Seattle mandated commercial and residential composting in an effort to reduce landfill waste. The ordinance enjoyed 74 percent approval.',
     source: <a href="https://www.foodabletv.com/blog/2016/2/21/seattles-mandated-composting-program-enlists-restaurants-to-reduce-waste-improve-efficiency-and-enhance-community"> Foodable </a>,
@@ -61,7 +61,7 @@ const blogData = [
   },
   {
     id: 7,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/Joy tastic.jpeg',
     title: ' How To Start Composting At Home',
     summary: ' Climate and location are factors in how you should compost, so be sure to research anything specific to your home, too. We\'re in Seattle, so our compost "recipe" reflects that. Let\'s dig in (pun very much intended).',
     source: <a href="https://jojotastic.com/2020/07/17/how-to-start-composting/"> Jojotastic </a>,
@@ -69,7 +69,7 @@ const blogData = [
   },
   {
     id: 8,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/King County Compost More.jpeg',
     title: ' Compost More. Waste Less.',
     summary: ' Did you know? Almost 35% of what ends up in the landfill is food scraps and food-soiled paper, and that it can be recycled in your curbside yardwaste cart? Don\'t waste this resource. Put it to a better use!',
     source: <a href="https://kingcounty.gov/depts/dnrp/solid-waste/garbage-recycling/compost-more.aspx"> King County </a>,
@@ -77,7 +77,7 @@ const blogData = [
   },
   {
     id: 9,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/Kirkland Conserves.jpeg',
     title: ' What Does Away Look Like? Where Kirkland\'s Trash, Recycling and Compost Go',
     summary: ' Every week we dutifully drag our three carts to the curb for pickup, and the materials we\'ve carefully sorted are taken away…but where, exactly, do they go?',
     source: <a href="http://cityofkirklandblogs.com/environmentalservices/2014/12/where-kirkland-trash-recycling-compost-goes/"> Kirkland Conserves </a>,
@@ -85,7 +85,7 @@ const blogData = [
   },
   {
     id: 10,
-    imageSrc: 'images/1.jpg',
+    imageSrc: 'blogImages/UWNews.jpeg',
     title: ' Composting food waste remains your best option, says UW study',
     summary: ' A new University of Washington study confirms that sentiment, and also calculates the environmental benefits associated with keeping these organic materials out of landfills.',
     source: <a href="https://www.washington.edu/news/2015/12/16/composting-food-waste-remains-your-best-option-says-uw-study/"> UW News </a>,
@@ -118,7 +118,7 @@ const VerticalCardList = () => {
                 }}
               >
                 <Grid container>
-                  <Grid item xs={12} sm={6} md={12}>
+                  <Grid item xs={12} sm={6} md={12} sx={{ borderRadius: '3px' }}> 
                     <ButtonBase sx={{ width: '100%', height: 128 }}>
                       <Img alt="complex" src={data.imageSrc} />
                     </ButtonBase>
